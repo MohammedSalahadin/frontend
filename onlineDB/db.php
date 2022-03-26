666b6ed93937e3655a9917fa77423df16ec0b6a6
mohamedmagdy <mohamedmagdy2999@gmail.com>
2022-03-26 21:06:06 +0200

    done it

--- onlineDB/db.php
+++ onlineDB/db.php
@@ -6,6 +6,7 @@ $dbPassword = '&8ck$f3!;[-]';
 $dbName = 'expressg_falcon';
 
 // Create connection for online database
+//using mysqli
 $mysqli = new mysqli($dbHost, $dbUsername, $dbPassword,$dbName);
 $mysqli->query("SET NAMES 'utf8'");
 $mysqli->query('SET CHARACTER SET utf8');
