25b257d96401cdf91d337567c7e9bc7b961e657d
MohammedSalahadin <mohammed.salahadinf18@komar.edu.iq>
2022-03-27 02:03:17 +0300

    I removed extra stuff, Zhiyar is confusing by them, He has to know we only finish front-end building here.....

--- routes/issues/monitor.php
+++ routes/issues/monitor.php
@@ -1,9 +1,6 @@
 <?php
-ob_start();
-if (isset($_SESSION['username'])) {
 
-  header("Location: index.php");
-}
+
 ?>
 
 <!DOCTYPE html>
@@ -16,18 +13,18 @@ if (isset($_SESSION['username'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="../assets/styles/styles.css">
+    <link rel="stylesheet" href="../../assets/styles/styles.css">
     <title>Falcontrac</title>
 </head>
 
 <body>
-    <?php require('../components/navbar.php') ?>
+    <?php require('../../components/navbar.php') ?>
 
-    <?php require('../components/menu.php') ?>
+    <?php require('../../components/menu.php') ?>
     <div class="d-flex flex-row-reverse overflow-auto">
 
         <?php
-    require('../components/monitor-table/monitorMessageBox.php')
+    require('../../components/monitor-table/monitorMessageBox.php')
     ?>
 
         <div class="container">
@@ -36,14 +33,14 @@ if (isset($_SESSION['username'])) {
             </div>
 
             <?php
-      require('../components/monitor-table/monitorTable.php')
+      require('../../components/monitor-table/monitorTable.php')
       ?>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
-    <script src="../assets/js/falcon.js"></script>
+    <script src="../../assets/js/falcon.js"></script>
 </body>
 
 </html>
